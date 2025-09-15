2b8c4d0bc9ec9e60954ba94351440dd3b964ec27
Arturo BUrigo <burigoarturo3@gmail.com>
2025-09-15 16:49:50 -0300

    ...

---
+++ formarmacao_arquitetura_java/ddd/src/main/java/br/com/alura/codechella/domain/evento/Evento.java
@@ -0,0 +1,39 @@
+package br.com.alura.codechella.domain.evento;
+
+import br.com.alura.codechella.domain.ingresso.TipoIngresso;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Evento {
+    private Categoria categoria;
+    private String descricao;
+    private LocalDateTime data;
+    private Endereco endereco;
+    private List<TipoIngresso> tipoIngressos = new ArrayList<>();
+
+    public void incluiNovoTipoIngressoAoEvento(TipoIngresso tipoIngresso) {
+        this.tipoIngressos.add(tipoIngresso);
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public List<TipoIngresso> getTipoIngressos() {
+        return tipoIngressos;
+    }
+}
